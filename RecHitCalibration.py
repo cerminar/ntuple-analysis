a18560738ca5c136a58087d3ae6d08387ce8bee4
Ziheng Chen <zihengchen@Zihengs-MacBook-Pro.local>
2017-08-21 20:13:07 +0200

    update calibration constant

--- RecHitCalibration.py
+++ RecHitCalibration.py
@@ -7,7 +7,7 @@ class RecHitCalibration:
 
     def __init__(self):
         """set variables used in the functions"""
-        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_0_X/RecoLocalCalo/HGCalRecProducers/python/HGCalRecHit_cfi.py#L6-L58
+        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_3_X/RecoLocalCalo/HGCalRecProducers/python/HGCalRecHit_cfi.py#L5
         self.dEdX_weights = (0.0,   # there is no layer zero
                              8.603,  # Mev
                              8.0675,
@@ -62,20 +62,20 @@ class RecHitCalibration:
                              92.196,
                              46.098)
 
-        #https://github.com/cms-sw/cmssw/blob/CMSSW_9_2_X/RecoLocalCalo/HGCalRecProducers/python/HGCalRecHit_cfi.py#L82
+        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_3_X/RecoLocalCalo/HGCalRecProducers/python/HGCalRecHit_cfi.py#L86
         self.thicknessCorrection = (1.132,1.092,1.084)  # 100, 200, 300 um
 
         # Base configurations for HGCal digitizers
-        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_2_X/SimCalorimetry/HGCalSimProducers/python/hgcalDigitizer_cfi.py#L5-L6
+        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_3_X/SimCalorimetry/HGCalSimProducers/python/hgcalDigitizer_cfi.py#L5
         # self.eV_per_eh_pair = 3.62
         self.fC_per_ele = 1.6020506e-4
         self.nonAgedNoises = (2100.0, 2100.0, 1600.0)  # 100,200,300 um (in electrons)
 
-        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_2_X/RecoLocalCalo/HGCalRecProducers/python/HGCalUncalibRecHit_cfi.py#L25
+        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_3_X/RecoLocalCalo/HGCalRecProducers/python/HGCalUncalibRecHit_cfi.py#L25
         self.fCPerMIP = (1.25, 2.57, 3.88)  # 100um, 200um, 300um
 
-        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_2_X/SimCalorimetry/HGCalSimProducers/python/hgcalDigitizer_cfi.py#L127
-        self.noise_MIP = 0.2
+        # https://github.com/cms-sw/cmssw/blob/CMSSW_9_3_X/SimCalorimetry/HGCalSimProducers/python/hgcalDigitizer_cfi.py#L127
+        self.noise_MIP = 1.0/7.0 #expectation based on latest SiPM performance
 
     def MeVperMIP(self, layer, thicknessIndex):
         if layer > 40:
