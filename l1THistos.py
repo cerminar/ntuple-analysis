b0ea555d340e808b593014ba1f931d8343578895
Gianluca <gianluca.cerminara@cern.ch>
2017-10-24 14:49:48 +0200

    remove boring code leveraging on inheritance and root capabilities

--- l1THistos.py
+++ l1THistos.py
@@ -3,6 +3,23 @@ import ROOT
 import root_numpy as rnp
 import numpy as np
 
+
+class BaseHistos():
+    def __init__(cls, name, root_file=None):
+        if root_file is not None:
+            for histo in root_file.GetListOfKeys():
+                if name in histo.GetName():
+                    # print histo.GetName(), histo.GetName().split('_')[2]
+                    setattr(cls, 'h_'+histo.GetName().split('_')[2], root_file.Get(histo.GetName()))
+        else:
+            for histo in [a for a in dir(cls) if a.startswith('h_')]:
+                getattr(cls, histo).Sumw2()
+
+    def write(cls):
+        for histo in [a for a in dir(cls) if a.startswith('h_')]:
+            getattr(cls, histo).Write()
+
+
 class GenPartHistos():
     def __init__(cls, name):
         cls.h_pt = ROOT.TH1F(name+'_pt', 'Gen Part Pt (GeV)', 100, 0, 100)
@@ -20,25 +37,35 @@ class GenPartHistos():
             getattr(cls, histo).Write()
 
 
-class TCHistos():
+class DigiHistos(BaseHistos):
     def __init__(cls, name, root_file=None):
         if not root_file:
-            cls.h_energy = ROOT.TH1F(name+'_energy', 'TC energy Pt (GeV)', 100, 0, 2)
+            cls.h_layer = ROOT.TH1F(name+'_layer', 'Digi layer #', 60, 0, 60)
+            cls.h_simenergy = ROOT.TH1F(name+'_energy', 'Digi sim-energy (GeV)', 100, 0, 2)
+        BaseHistos.__init__(cls, name, root_file)
+
+    def fill(cls, digis):
+        rnp.fill_hist(cls.h_layer, digis.layer)
+        rnp.fill_hist(cls.h_simenergy, digis.simenergy)
+
+
+class TCHistos(BaseHistos):
+    def __init__(cls, name, root_file=None):
+        if not root_file:
+            cls.h_energy = ROOT.TH1F(name+'_energy', 'TC energy (GeV)', 100, 0, 2)
             cls.h_subdet = ROOT.TH1F(name+'_subdet', 'TC subdet #', 8, 0, 8)
             cls.h_layer = ROOT.TH1F(name+'_layer', 'TC layer #', 60, 0, 60)
             cls.h_absz = ROOT.TH1F(name+'_absz', 'TC z(cm)', 100, 300, 500)
             cls.h_wafertype = ROOT.TH1F(name+'_wafertype', 'Wafer type', 10, 0, 10)
-            cls.h_layerVenergy = ROOT.TH2F(name+'_layerVenergy', "Energy (GeV) vs Layer #", 100, 0, 2, 50, 0, 50)
+            cls.h_layerVenergy = ROOT.TH2F(name+'_layerVenergy', "Energy (GeV) vs Layer #", 60, 0, 60, 100, 0, 2)
+            cls.h_energyVeta = ROOT.TH2F(name+'_energyVeta', "Energy (GeV) vs Eta", 100, -3.5, 3.5, 100, 0, 2)
+            cls.h_energyVetaL1t5 = ROOT.TH2F(name+'_energyVetaL1t5', "Energy (GeV) vs Eta (layers 1 to 5)", 100, -3.5, 3.5, 100, 0, 2)
+            cls.h_energyVetaL6t10 = ROOT.TH2F(name+'_energyVetaL6t10', "Energy (GeV) vs Eta (layers 6 to 10)", 100, -3.5, 3.5, 100, 0, 2)
+            cls.h_energyVetaL11t20 = ROOT.TH2F(name+'_energyVetaL11t20', "Energy (GeV) vs Eta (layers 11 to 20)", 100, -3.5, 3.5, 100, 0, 2)
+            cls.h_energyVetaL21t60 = ROOT.TH2F(name+'_energyVetaL21t60', "Energy (GeV) vs Eta (layers 21 to 60)", 100, -3.5, 3.5, 100, 0, 2)
+            cls.h_energyPetaVphi = ROOT.TProfile2D(name+'_energyPetaVphi', "Energy profile (GeV) vs Eta and Phi", 100, -3.5, 3.5, 100, -3.2, 3.2)
 
-            for histo in [a for a in dir(cls) if a.startswith('h_')]:
-                getattr(cls, histo).Sumw2()
-        else:
-            cls.h_energy = root_file.Get(name+'_energy')
-            cls.h_subdet = root_file.Get(name+'_subdet')
-            cls.h_layer = root_file.Get(name+'_layer')
-            cls.h_absz = root_file.Get(name+'_absz')
-            cls.h_wafertype = root_file.Get(name+'_wafertype')
-            cls.h_layerVenergy = root_file.Get(name+'_layerVenergy')
+        BaseHistos.__init__(cls, name, root_file)
 
     def fill(cls, tcs):
         rnp.fill_hist(cls.h_energy, tcs.energy)
@@ -47,16 +74,18 @@ class TCHistos():
         rnp.fill_hist(cls.h_absz, np.fabs(tcs.z))
         rnp.fill_hist(cls.h_wafertype, tcs.wafertype)
         rnp.fill_hist(cls.h_wafertype, tcs.wafertype)
-        rnp.fill_hist(cls.h_layerVenergy, tcs[['energy', 'layer']])
+        # FIXME: should bin this guy in eta bins
+        rnp.fill_hist(cls.h_layerVenergy, tcs[['layer', 'energy']])
+        rnp.fill_hist(cls.h_energyVeta, tcs[['eta', 'energy']])
+        rnp.fill_hist(cls.h_energyVeta, tcs[['eta', 'energy']])
+        rnp.fill_hist(cls.h_energyVetaL1t5, tcs[(tcs.layer >= 1) & (tcs.layer <= 5)][['eta', 'energy']])
+        rnp.fill_hist(cls.h_energyVetaL6t10, tcs[(tcs.layer >= 6) & (tcs.layer <= 10)][['eta', 'energy']])
+        rnp.fill_hist(cls.h_energyVetaL11t20, tcs[(tcs.layer >= 11) & (tcs.layer <= 20)][['eta', 'energy']])
+        rnp.fill_hist(cls.h_energyVetaL21t60, tcs[(tcs.layer >= 21) & (tcs.layer <= 60)][['eta', 'energy']])
+        rnp.fill_profile(cls.h_energyPetaVphi, tcs[['eta', 'phi', 'energy']])
 
-        # cls.h_layerVenergy.Fill(energy, layer, weight)
 
-    def write(cls):
-        for histo in [a for a in dir(cls) if a.startswith('h_')]:
-            getattr(cls, histo).Write()
-
-
-class ClusterHistos():
+class ClusterHistos(BaseHistos):
     def __init__(cls, name, root_file=None):
         if not root_file:
             cls.h_energy = ROOT.TH1F(name+'_energy', 'Cluster energy Pt (GeV)', 100, 0, 20)
@@ -64,15 +93,7 @@ class ClusterHistos():
             cls.h_ncells = ROOT.TH1F(name+'_ncells', 'Cluster # cells', 30, 0, 30)
             cls.h_layerVenergy = ROOT.TH2F(name+'_layerVenergy', "Cluster Energy (GeV) vs Layer #", 100, 0, 20, 50, 0, 50)
             cls.h_layerVncells = ROOT.TH2F(name+'_layerVncells', "Cluster #cells vs Layer #", 30, 0, 30, 50, 0, 50)
-
-            for histo in [a for a in dir(cls) if a.startswith('h_')]:
-                getattr(cls, histo).Sumw2()
-        else:
-            cls.h_energy = root_file.Get(name+'_energy')
-            cls.h_layer = root_file.Get(name+'_layer')
-            cls.h_ncells = root_file.Get(name+'_ncells')
-            cls.h_layerVenergy = root_file.Get(name+'_layerVenergy')
-            cls.h_layerVncells = root_file.Get(name+'_layerVncells')
+        BaseHistos.__init__(cls, name, root_file)
 
     def fill(cls, clsts):
         rnp.fill_hist(cls.h_energy, clsts.energy)
@@ -81,18 +102,14 @@ class ClusterHistos():
         rnp.fill_hist(cls.h_layerVenergy, clsts[['energy', 'layer']])
         rnp.fill_hist(cls.h_layerVncells, clsts[['ncells', 'layer']])
 
-    def write(cls):
-        for histo in [a for a in dir(cls) if a.startswith('h_')]:
-            getattr(cls, histo).Write()
 
-
-class Cluster3DHistos():
+class Cluster3DHistos(BaseHistos):
     def __init__(cls, name, root_file=None):
         if not root_file:
             cls.h_pt = ROOT.TH1F(name+'_pt', '3D Cluster Pt (GeV)', 100, 0, 100)
             cls.h_energy = ROOT.TH1F(name+'_energy', '3D Cluster energy (GeV)', 100, 0, 100)
             cls.h_nclu = ROOT.TH1F(name+'_nclu', '3D Cluster # clusters', 30, 0, 30)
-            cls.h_showlenght = ROOT.TH1F(name+'_showlenght', '3D Cluster showerlenght', 30, 0, 30)
+            cls.h_showlenght = ROOT.TH1F(name+'_showlenght', '3D Cluster showerlenght', 60, 0, 60)
             cls.h_firstlayer = ROOT.TH1F(name+'_firstlayer', '3D Cluster first layer', 30, 0, 30)
             cls.h_sEtaEtaTot = ROOT.TH1F(name+'_sEtaEtaTot', '3D Cluster RMS Eta', 100, 0, 0.1)
             cls.h_sEtaEtaMax = ROOT.TH1F(name+'_sEtaEtaMax', '3D Cluster RMS Eta (max)', 100, 0, 0.1)
@@ -100,25 +117,7 @@ class Cluster3DHistos():
             cls.h_sPhiPhiMax = ROOT.TH1F(name+'_sPhiPhiMax', '3D Cluster RMS Phi (max)', 100, 0, 2)
             cls.h_sZZ = ROOT.TH1F(name+'_sZZ', '3D Cluster RMS Z ???', 100, 0, 10)
             cls.h_eMaxOverE = ROOT.TH1F(name+'_eMaxOverE', '3D Cluster Emax/E', 100, 0, 1)
-
-            for histo in [a for a in dir(cls) if a.startswith('h_')]:
-                getattr(cls, histo).Sumw2()
-        else:
-            cls.h_pt
-            cls.h_energy
-            cls.h_nclu
-            cls.h_showlenght
-            cls.h_firstlayer
-            cls.h_sEtaEtaTot
-            cls.h_sEtaEtaMax
-            cls.h_sPhiPhiTot
-            cls.h_sPhiPhiMax
-            cls.h_sZZ
-            cls.h_eMaxOverE
-
-            for histo in [a for a in dir(cls) if a.startswith('h_')]:
-                h_method = getattr(cls, histo)
-                h_method = root_file.Get(name+'_'+histo.split('_')[1])
+        BaseHistos.__init__(cls, name, root_file)
 
     def fill(cls, cl3ds):
         rnp.fill_hist(cls.h_pt, cl3ds.pt)
@@ -132,7 +131,3 @@ class Cluster3DHistos():
         rnp.fill_hist(cls.h_sPhiPhiMax, cl3ds.sppmax)
         rnp.fill_hist(cls.h_sZZ, cl3ds.szz)
         rnp.fill_hist(cls.h_eMaxOverE, cl3ds.emaxe)
-
-    def write(cls):
-        for histo in [a for a in dir(cls) if a.startswith('h_')]:
-            getattr(cls, histo).Write()
