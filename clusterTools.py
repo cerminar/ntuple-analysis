a8d8bde1d79d641b859fa8b78ac7011a4ca056af
Gianluca <gianluca.cerminara@cern.ch>
2017-11-27 17:50:08 +0100

    bigger radius for 2D and add some core variables

--- clusterTools.py
+++ clusterTools.py
@@ -21,13 +21,18 @@ def buildDBSCANClusters(sel_layer, sel_zside, tcs):
 
     X = tcs_layer[['x', 'y']]
     densities = [0.1*100, 0.2*100, 0.5*100, 0.7*100, 1.1*100, 1.3*100, 1.7*100, 1.8*100, 2.0*100, 2.2*100, 2.6*100, 2.0*100, 1.8*100, 1.4*100, 1.2*100, 0.8*100, 0.6*100, 0.4*100, 0.2*100, 0.2*100, 0.1*100, 0.05*100, 0.05*100, 0.05*100, 0.05*100, 0.05*100, 0.05*100, 0.05*100]
-    db = DBSCAN(eps=3.5,
+    db = DBSCAN(eps=3.6,
                 min_samples=densities[sel_layer-1],
                 algorithm='kd_tree',
                 n_jobs=3).fit(X, sample_weight=tcs_layer['energy']*100)
     labels = db.labels_
     unique_labels = set(labels)
     tcs_layer['dbs_label'] = labels
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    tcs_layer['core'] = core_samples_mask
+    # print db.core_sample_indices_
+    # print tcs_layer
 
     for label in unique_labels:
         if label == -1:
@@ -35,6 +40,8 @@ def buildDBSCANClusters(sel_layer, sel_zside, tcs):
         components = tcs_layer[tcs_layer.dbs_label == label]
         cl = pd.DataFrame()
         cl['energy'] = [components.energy.sum()]
+
+        cl['energyCore'] = [components[components.core].energy.sum()]
         cl['x'] = [np.sum(components.x*components.energy)/components.energy.sum()]
         cl['y'] = [np.sum(components.y*components.energy)/components.energy.sum()]
         cl['z'] = [components.z.iloc[0]]
@@ -49,6 +56,7 @@ def buildDBSCANClusters(sel_layer, sel_zside, tcs):
         #     cl['phi'] = [0]
         cl['cells'] = [np.array(components.index)]
         cl['ncells'] = [components.shape[0]]
+        cl['nCoreCells'] = [components[components.core].shape[0]]
         new2Dcls = new2Dcls.append(cl.copy(), ignore_index=True)
     return new2Dcls
 
@@ -73,6 +81,9 @@ def build3DClustersEtaPhi(cl2D):
         calib_factor = 1.084
         components = cl2D[cl2D.dbs_labels == label]
         cl3D['energy'] = [components.energy.sum()*calib_factor]
+        cl3D['energyCore'] = [components.energyCore.sum()*calib_factor]
+        cl3D['energyCentral'] = [components[(components.layer > 9) & (components.layer < 21)].energy.sum()*calib_factor]
+
         # print components
         cl3D['eta'] = [np.sum(components.eta*components.energy)/components.energy.sum()]
         cl3D['phi'] = [np.sum(components.phi*components.energy)/components.energy.sum()]
