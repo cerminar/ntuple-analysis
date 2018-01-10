101a826558b8ea4ab8392f21f1062e548dfaf789
Gianluca <gianluca.cerminara@cern.ch>
2018-01-10 09:58:51 +0100

    small fixes

--- templates/run_batch.sh
+++ templates/run_batch.sh
@@ -2,9 +2,13 @@
 CLUSTERID=$1
 PROCID=$2
 
+echo "Runnin Cluster ${CLUSTERID} Job ${PROCID}"
+echo "Current dir: ${PWD}"
+cd TEMPL_WORKDIR
+echo "Now in dir: ${PWD}"
 
-source setup_lxplus.sh
+source ./setup_lxplus.sh
 source ~/scripts/setVirtualEnvWrapper.sh
 workon HGCTPGPerformance-1
-cd TEMPL_WORKDIR
+
 python analyzeHgcalL1Tntuple.py -f TEMPL_CFG -c TEMPL_COLL -s ${PROCID} -n -1
