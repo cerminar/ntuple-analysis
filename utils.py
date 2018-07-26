0bbc404414e5d86ef8749de0caa1d2458fab9b79
Gianluca Cerminara <gianluca.cerminara@cern.ch>
2018-07-26 17:31:46 +0200

    now return the index and not the position in the array

--- utils.py
+++ utils.py
@@ -4,16 +4,21 @@ from scipy.spatial import cKDTree
 
 
 def match_etaphi(ref_etaphi, trigger_etaphi, trigger_pt, deltaR=0.2):
-    '''Match object with the highest pT within a given DeltaR'''
+    '''Match objects within a given DeltaR. Returns the panda index of the best match (highest-pt)
+       and of all the matches'''
+    # print "INPUT ref_etaphi"
+    # print ref_etaphi
+    # print "INPUT trigger_etaphi"
+    # print trigger_etaphi
+    # print "INPUT trigger_pt"
+    # print trigger_pt
     kdtree = cKDTree(trigger_etaphi)
-    matched_indices = {}
-    allmatches = {}
+    best_match_indices = {}
+    all_matches_indices = {}
     # for iref,(eta,phi) in enumerate(ref_etaphi):
     for index, row in ref_etaphi.iterrows():
-        # print (index)
-        # print (row)
         matched = kdtree.query_ball_point([row.eta, row.phi], deltaR)
-
+        # not this in an integer of the index of the array not the index in the pandas meaning: hence to beused with iloc
         # Handle the -pi pi transition
         matched_sym = kdtree.query_ball_point([row.eta, row.phi-np.sign(row.phi)*2.*m.pi], deltaR)
         matched = np.unique(np.concatenate((matched, matched_sym))).astype(int)
@@ -23,11 +28,12 @@ def match_etaphi(ref_etaphi, trigger_etaphi, trigger_pt, deltaR=0.2):
         # print trigger_etaphi.iloc[matched]
         # Choose the match with highest pT
         if (len(matched) != 0):
-            best_match = np.argmax(trigger_pt[matched])
-            # print best_match
-            matched_indices[index] = best_match
-            allmatches[index] = matched
-    return matched_indices, allmatches
+            best_match = np.argmax(trigger_pt.iloc[matched])
+            best_match_indices[index] = best_match
+            all_matches_indices[index] = trigger_pt.iloc[matched].index.values
+    # print best_match_indices
+    # print all_matches_indices
+    return best_match_indices, all_matches_indices
 
 
 def debugPrintOut(level, name, toCount, toPrint):
