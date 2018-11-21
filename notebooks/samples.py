9606fe66388a42ec5db61327d2338fc4ead00e52
Gianluca Cerminara <gianluca.cerminara@cern.ch>
2018-11-21 15:25:39 +0100

    Update

--- notebooks/samples.py
+++ notebooks/samples.py
@@ -1,10 +1,10 @@
 import ROOT
+import pandas as pd
 
 version = 'v53'
 
 files = {}
 file_keys = {}
-
 
 
 class RootFile:
@@ -28,16 +28,18 @@ class RootFile:
         self._file_keys = file_keys[self.file_name]
         return self._file_keys
 
+
 class Sample():
-    def __init__(cls, name, label, version=None):
-        cls.name = name
-        cls.label = label
+    def __init__(self, name, label, version=None, type=None):
+        self.name = name
+        self.label = label
         if version:
             version = '_'+version
         else:
             version = ''
-        cls.histo_filename = '../plots1/histos_{}{}.root'.format(cls.name, version)
-        cls.histo_file = ROOT.TFile(cls.histo_filename, 'r')#RootFile(cls.histo_filename)
+        self.histo_filename = '../plots1/histos_{}{}.root'.format(self.name, version)
+        self.histo_file = ROOT.TFile(self.histo_filename, 'r')
+        self.type = type
 
 
 # sample_names = ['ele_flat2to100_PU0',
@@ -53,7 +55,6 @@ def get_label_dict(selections):
     return dictionary
 
 
-
 class HProxy:
     def __init__(self, classtype, tp, tp_sel, gen_sel, root_file):
         self.classtype = classtype
@@ -63,14 +64,14 @@ class HProxy:
         self.root_file = root_file
         self.instance = None
 
-
     def get(self):
         if self.instance is None:
             name = '{}_{}_{}'.format(self.tp, self.tp_sel, self.gen_sel)
-            if self.gen_sel == None:
+            if self.gen_sel is None:
                 name = '{}_{}'.format(self.tp, self.tp_sel)
             self.instance = self.classtype(name, self.root_file)
         return self.instance
+
 
 class HPlot:
     def __init__(self, samples, tp_sets, tp_selections, gen_selections):
@@ -78,10 +79,13 @@ class HPlot:
         self.tp_selections = tp_selections
         self.gen_selections = gen_selections
         self.pus = []
+        self.labels_dict = {}
+
         for sample in samples:
             self.pus.append(sample.label)
+            self.labels_dict[sample.type] = sample.type
+
         self.data = pd.DataFrame(columns=['sample', 'pu', 'tp', 'tp_sel', 'gen_sel', 'classtype', 'histo'])
-        self.labels_dict = {}
 
         self.labels_dict.update(tp_sets)
         self.labels_dict.update(tp_selections)
@@ -95,32 +99,23 @@ class HPlot:
                     tps,
                     tp_sels,
                     gen_sels):
+        if gen_sels is None:
+            gen_sels = [None]
+
         for sample in samples:
             print sample
             for tp in tps:
                 for tp_sel in tp_sels:
-                    if gen_sels is None:
-                        self.data = self.data.append({'sample': 'ele',
-                                                        'pu': sample.label,
-                                                        'tp': tp,
-                                                        'tp_sel': tp_sel,
-                                                        'gen_sel': None,
-                                                        'classtype': classtype,
-                                                        'histo': HProxy(classtype, tp, tp_sel, None, sample.histo_file)}
-                                                          , ignore_index=True)
-                    else:
-                        for gen_sel in gen_sels:
-                            print sample, tp, tp_sel, gen_sel
-                            self.data = self.data.append({'sample': 'ele',
-                                                            'pu': sample.label,
-                                                            'tp': tp,
-                                                            'tp_sel': tp_sel,
-                                                            'gen_sel': gen_sel,
-                                                            'classtype': classtype,
-                                                            'histo': HProxy(classtype, tp, tp_sel, gen_sel, sample.histo_file)}
-                                                              , ignore_index=True)
-
-
+                    for gen_sel in gen_sels:
+                        print sample, tp, tp_sel, gen_sel
+                        self.data = self.data.append({'sample': sample.type,
+                                                      'pu': sample.label,
+                                                      'tp': tp,
+                                                      'tp_sel': tp_sel,
+                                                      'gen_sel': gen_sel,
+                                                      'classtype': classtype,
+                                                      'histo': HProxy(classtype, tp, tp_sel, gen_sel, sample.histo_file)},
+                                                     ignore_index=True)
 
     def get_histo(self,
                   classtype,
@@ -132,38 +127,41 @@ class HPlot:
         histo = None
         labels = []
         text = ''
+
         query = '(pu == @pu) & (tp == @tp) & (tp_sel == @tp_sel) & (classtype == @classtype)'
         if gen_sel is not None:
             query += '& (gen_sel == @gen_sel)'
+        if sample is not None:
+            query += '& (sample == @sample)'
+
         histo_df = self.data.query(query)
 
         if histo_df.empty:
             print 'No match found for: pu: {}, tp: {}, tp_sel: {}, gen_sel: {}, classtype: {}'.format(pu, tp, tp_sel, gen_sel, classtype)
-
+            return None, None, None
+#         print histo_df
 
         field_counts = histo_df.apply(lambda x: len(x.unique()))
         label_fields = []
         text_fields = []
         # print field_counts
         for field in field_counts.iteritems():
-            if(field[1] > 1 and field[0] != 'histo' and field[0] != 'sample'):
+            if(field[1] > 1 and field[0] != 'histo'):
                 label_fields.append(field[0])
             if(field[1] == 1 and field[0] != 'histo' and field[0] != 'classtype' and field[0] != 'sample'):
                 if(gen_sel is None and field[0] == 'gen_sel'):
                     continue
                 text_fields.append(field[0])
-
 #         print 'label fields: {}'.format(label_fields)
 #         print 'text fields: {}'.format(text_fields)
 
         for item in histo_df[label_fields].iterrows():
             labels.append(', '.join([self.labels_dict[tx] for tx in item[1].values if self.labels_dict[tx] != '']))
 
-        #print labels
+        # print labels
         text = ', '.join([self.labels_dict[fl] for fl in histo_df[text_fields].iloc[0].values if self.labels_dict[fl] != ''])
-        histo = [his.get() for his in  histo_df['histo'].values]
+        histo = [his.get() for his in histo_df['histo'].values]
         return histo, labels, text
-
 
 
 # -------------------------------------------------------------------------
@@ -197,6 +195,8 @@ samples_nugunrates = [sample_nugunrate]
 from python.selections import tp_rate_selections
 from python.selections import tp_match_selections
 from python.selections import gen_part_selections
+from python.selections import eg_rate_selections
+from python.selections import eg_qual_selections
 
 tpsets = {'DEF': 'NNDR',
           'DEFCalib': 'NNDR Calib v1'}
