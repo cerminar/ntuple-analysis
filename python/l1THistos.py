60b15f683a573509d55f59e89a691c49a59a9b0e
Gianluca Cerminara <gianluca.cerminara@cern.ch>
2019-10-21 15:00:53 +0200

    Fix abs eta for barrel + endcap

--- python/l1THistos.py
+++ python/l1THistos.py
@@ -172,7 +172,7 @@ class GenParticleHistos(BaseHistos):
     def __init__(self, name, root_file=None, debug=False):
         if not root_file:
             self.h_eta = ROOT.TH1F(name+'_eta', 'Gen Part eta; #eta^{GEN};', 50, -3, 3)
-            self.h_abseta = ROOT.TH1F(name+'_abseta', 'Gen Part |eta|; |#eta^{GEN}|;', 16, 1.4, 3)
+            self.h_abseta = ROOT.TH1F(name+'_abseta', 'Gen Part |eta|; |#eta^{GEN}|;', 40, 0, 4)
             self.h_pt = ROOT.TH1F(name+'_pt', 'Gen Part P_{T} (GeV); p_{T}^{GEN} [GeV];', 50, 0, 100)
             self.h_energy = ROOT.TH1F(name+'_energy', 'Gen Part Energy (GeV); E [GeV];', 100, 0, 1000)
             self.h_reachedEE = ROOT.TH1F(name+'_reachedEE', 'Gen Part reachedEE', 4, 0, 4)
