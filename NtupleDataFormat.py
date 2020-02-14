01aa8d79a724027dc267fb6d198b673cdda76dd8
Gianluca Cerminara <gianluca.cerminara@cern.ch>
2020-02-14 09:42:34 +0100

    Streamline tree reading code using root_numpy

--- NtupleDataFormat.py
+++ NtupleDataFormat.py
@@ -27,6 +27,15 @@ class HGCalNtuple(object):
         """
         super(HGCalNtuple, self).__init__()
         self._tree = ROOT.TChain(tree)
+        self._branches = []
+        branch_blacklist = ['tc_wafer',
+                            'tc_cell',
+                            'tc_waferu',
+                            'tc_waferv',
+                            'tc_cellu',
+                            'tc_cellv',
+                            'gen_PUNumInt',
+                            'gen_TrueNumInt']
 
         for file_name in fileNames:
             protocol = ''
@@ -36,7 +45,8 @@ class HGCalNtuple(object):
                 protocol = 'root://eoscms.cern.ch/'
 
             self._tree.Add(protocol+file_name)
-
+            if len(self._branches) == 0:
+                self._branches = [br for br in rnp.list_branches(protocol+file_name, tree) if br not in branch_blacklist]
         # print 'Cache size: {}'.format(self._tree.GetCacheSize())
 
         self._entries = self._tree.GetEntries()
@@ -87,7 +97,7 @@ class HGCalNtuple(object):
             if nb <= 0:
                 continue
 
-            yield Event(self._tree, jentry)
+            yield Event(self._tree, jentry, self._branches)
 
     def getEvent(self, index):
         """Returns Event for a given index"""
@@ -98,7 +108,7 @@ class HGCalNtuple(object):
         # if nb <= 0:
         #     None
 
-        return Event(self._tree, index)  # ientry of jentry?
+        return Event(self._tree, index, self._branches)  # ientry of jentry?
 
 
 ##########
@@ -109,7 +119,7 @@ class Event(object):
     or collections of objects.
     """
 
-    def __init__(self, tree, entry):
+    def __init__(self, tree, entry, branches):
         """Constructor.
 
         Arguments:
@@ -119,6 +129,7 @@ class Event(object):
         super(Event, self).__init__()
         self._tree = tree
         self._entry = entry
+        self._branches = branches
 
     def entry(self):
         return self._entry
@@ -143,7 +154,7 @@ class Event(object):
         """Returns 'run:lumi:event' string."""
         return "%d:%d:%d" % self.eventId()
 
-    def getDataFrame(self, prefix):
+    def getDataFrame2(self, prefix):
         # print prefix
         branch_blacklist = ['tc_wafer', 'tc_cell', 'tc_waferu', 'tc_waferv', 'tc_cellu', 'tc_cellv']
 
@@ -164,6 +175,28 @@ class Event(object):
 
             df[names[idx]] = nd_array[branch][0]
         return df
+
+    def getDataFrame(self, prefix):
+
+        branches = [br for br in self._branches
+                    if br.startswith(prefix+'_') and
+                    not br == '{}_n'.format(prefix)]
+        names = ['_'.join(br.split('_')[1:]) for br in branches]
+
+        if len(branches) == 0:
+            return pd.DataFrame()
+
+        nd_array = rnp.tree2array(self._tree,
+                                  branches=branches,
+                                  start=self._entry,
+                                  stop=self._entry+1,
+                                  cache_size=400000000)
+
+        df = pd.DataFrame(columns=names)
+        for idx, branch in enumerate(branches):
+            df[names[idx]] = nd_array[branch][0]
+        return df
+
 
     def getPUInfo(self):
         branches = ['gen_PUNumInt', 'gen_TrueNumInt']
