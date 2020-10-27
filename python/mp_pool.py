d142d10a260750e2d6df0e58269b7400a587d65a
Gianluca Cerminara <gianluca.cerminara@cern.ch>
2020-10-27 09:30:52 +0100

    switch off multiprocessing for now

--- python/mp_pool.py
+++ python/mp_pool.py
@@ -1,2 +1,13 @@
-from multiprocessing import Pool
-POOL = Pool(5)
+# from multiprocessing import Pool
+# POOL = Pool(5)
+
+# FIXME: dummy to avoid the complication of multiprocessing for now
+class Pool(object):
+    def map(self, func, args):
+        ret = []
+        for arg in args:
+            ret.append(func(args))
+        return ret
+
+
+POOL = Pool()
