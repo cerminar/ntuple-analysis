a3aac5fcc3cf60dfe6a0be8110ee862601b34ede
Gianluca <gianluca.cerminara@cern.ch>
2020-10-28 10:16:15 +0100

    more recent version to use py3.8 and move to root 6.22

--- setup_lxplus.sh
+++ setup_lxplus.sh
@@ -8,9 +8,10 @@
 # source ${CVMFS_TOOLS}/external/tbb/2017_U6/etc/profile.d/init.sh
 # source ${CVMFS_TOOLS}/external/xrootd/4.5.0/etc/profile.d/init.sh
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd /cvmfs/cms.cern.ch/slc7_amd64_gcc700/cms/cmssw/CMSSW_10_2_0/src/
+cd /cvmfs/cms.cern.ch/slc7_amd64_gcc820/cms/cmssw/CMSSW_11_1_2/src/
 echo $?
 eval `scram runtime -sh`
 cd -
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc820/lcg/root/6.22.03/bin/thisroot.sh
 export VIRTUALENVWRAPPER_PYTHON=`which python`
 export VIRTUALENV_PYTHON=`which python`
