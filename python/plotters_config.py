9a333464dc059eccaefa9887f25ed6bb26fe75a3
cerminar <gianluca.cerminara@cern.ch>
2021-08-02 12:33:29 +0200

    tune

--- python/plotters_config.py
+++ python/plotters_config.py
@@ -216,14 +216,13 @@ eg_genmatched_plotters_iso = [
 ]
 
 
-# NOTE: collections and selections have been revised (and trimmed)
 eg_genmatched_plotters = [
-    plotters.EGGenMatchPlotter(
-        collections.egs_EE, collections.gen_parts,
-        selections.simeg_match_ee_selections, selections.gen_ee_selections),
-    plotters.EGGenMatchPlotter(
-        collections.egs_EB, collections.gen_parts,
-        selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
+    # plotters.EGGenMatchPlotter(
+    #     collections.egs_EE, collections.gen_parts,
+    #     selections.simeg_match_ee_selections, selections.gen_ee_selections),
+    # plotters.EGGenMatchPlotter(
+    #     collections.egs_EB, collections.gen_parts,
+    #     selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
     # plotters.EGGenMatchPlotter(
     #     collections.egs_EE_pf, collections.gen_parts,
     #     selections.eg_id_pt_ee_selections, selections.gen_ee_selections),
@@ -272,7 +271,12 @@ eg_genmatched_plotters = [
     plotters.EGGenMatchPlotter(
         collections.tkem_EB_pfnf, collections.gen_parts,
         selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
-
+    plotters.DecTrackGenMatchPlotter(
+        collections.decTk,
+        collections.gen_parts,
+        selections.dectk_match_selections,
+        selections.gen_ee_tk_selections
+    )
 ]
 
 
