75642c4d82d73928a89482a66620c0b700449315
cerminar <gianluca.cerminara@cern.ch>
2021-08-17 11:47:09 +0200

    update

--- setVirtualEnvWrapper.sh
+++ setVirtualEnvWrapper.sh
@@ -1,5 +1,6 @@
 export WORKON_HOME=~/.virtualenvs
 export PATH=~/.local/bin/:${PATH}
 export VIRTUALENVWRAPPER_PYTHON=`which python`
+export VIRTUALENV_PYTHON=`which python`
 export VIRTUALENVWRAPPER_VIRTUALENV=`which virtualenv`
 source virtualenvwrapper.sh
