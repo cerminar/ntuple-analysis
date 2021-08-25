28df2c6af539aa0c49c44f1d363c5e96e07c7a9a
cerminar <gianluca.cerminara@cern.ch>
2021-08-25 17:16:59 +0200

    Allow to dump selections and plotters lists by name

--- python/plotters_config.py
+++ python/plotters_config.py
@@ -1,3 +1,4 @@
+# run with python -m python.plotters_config
 from __future__ import absolute_import
 from . import plotters
 from . import collections
@@ -483,3 +484,14 @@ decTk_plotters = [
         selections.gen_ee_tk_selections
     )
 ]
+
+
+
+if __name__ == "__main__":
+
+    print('enter name: ')
+    selec_name = input()
+    sel_list = []
+    sel_list = eval(selec_name)
+    for sel in sel_list:
+        print(sel)
