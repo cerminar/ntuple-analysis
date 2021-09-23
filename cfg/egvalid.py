b006cc7027fbb79e67c7b4a551814cac9b89e1e1
cerminar <gianluca.cerminara@cern.ch>
2021-09-23 15:17:42 +0200

    Add plotters for more fine grained control of EG validation

---
+++ cfg/egvalid.py
@@ -0,0 +1,70 @@
+from __future__ import absolute_import
+import python.plotters as plotters
+import python.collections as collections
+import python.selections as selections
+
+sim_eg_match_ee_selections = (selections.Selector('^EGq[4-5]$')*('^Pt[1-3][0]$|all'))()
+
+tdrsim_eg_genmatched = [
+    #     collections.egs_EE, collections.gen_parts,
+    #     sim_eg_match_ee_selections, selections.gen_ee_selections),
+    # plotters.EGGenMatchPlotter(
+    #     collections.egs_EB, collections.gen_parts,
+    #     selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkeles_EE, collections.gen_parts,
+        sim_eg_match_ee_selections, selections.gen_ee_tk_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkeles_EB, collections.gen_parts,
+        selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkem_EE, collections.gen_parts,
+        sim_eg_match_ee_selections, selections.gen_ee_tk_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkem_EB, collections.gen_parts,
+        selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
+]
+
+
+l1tc_match_ee_selections = (selections.Selector('^EGq[1,2]$')*('^Pt[1-2][0]$|all'))()
+
+l1tc_eg_genmatched = [
+    plotters.EGGenMatchPlotter(
+        collections.egs_EE_pfnf, collections.gen_parts,
+        l1tc_match_ee_selections, selections.gen_ee_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkeles_EE_pfnf, collections.gen_parts,
+        l1tc_match_ee_selections, selections.gen_ee_tk_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkeles_EB_pfnf, collections.gen_parts,
+        selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkem_EE_pfnf, collections.gen_parts,
+        l1tc_match_ee_selections, selections.gen_ee_tk_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkem_EB_pfnf, collections.gen_parts,
+        selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
+
+]
+
+l1tc_fw_match_ee_selections = (selections.Selector('^EGq[2,4]or[3,5]$')*('^Pt[1-2][0]$|all'))()
+
+
+l1tc_fw_eg_genmatched = [
+    plotters.EGGenMatchPlotter(
+        collections.egs_EE_pfnf, collections.gen_parts,
+        l1tc_fw_match_ee_selections, selections.gen_ee_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkeles_EE_pfnf, collections.gen_parts,
+        l1tc_fw_match_ee_selections, selections.gen_ee_tk_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkeles_EB_pfnf, collections.gen_parts,
+        selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkem_EE_pfnf, collections.gen_parts,
+        l1tc_fw_match_ee_selections, selections.gen_ee_tk_selections),
+    plotters.EGGenMatchPlotter(
+        collections.tkem_EB_pfnf, collections.gen_parts,
+        selections.eg_id_pt_eb_selections, selections.gen_eb_selections),
+
+]
