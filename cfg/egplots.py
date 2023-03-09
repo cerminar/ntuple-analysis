dbe8f9e00693ca8542e101997426b99dc56e2023
Gianluca <gianluca.cerminara@cern.ch>
2023-03-09 11:49:55 +0100

    Add quantization plots

--- cfg/egplots.py
+++ cfg/egplots.py
@@ -9,9 +9,28 @@ import python.selections as selections
 simple_selections = [selections.Selection("all", '', ''),
                      selections.Selection('Pt10', 'p_{T}^{TOBJ}>=10GeV', 'pt >= 10'),]
 
-print(f"simple_selections: {simple_selections}")
+sta_selection = (selections.Selector('^IDTight[EPS]|all')*selections.Selector('^Pt5|all')*selections.Selector('^EtaABCDF$|all'))()
+# print(f"simple_selections: {simple_selections}")
+
+egid_iso_etatk_selections = (selections.Selector('^IDTight[EP]|all')*selections.Selector('^Iso|all')*selections.Selector('^Eta[F]$|^Eta[AF][ABCD]*[C]$'))()
 
 l1tc_simple_plotters = [
     # EE Tk-electrons
-    plotters.TkElePlotter(collections.tkeles_EE_pfnf, simple_selections),
+    # plotters.TkElePlotter(collections.TkEleEE, egid_iso_etatk_selections),
+    # plotters.TkElePlotter(collections.TkEleEB, egid_iso_etatk_selections),
+    # plotters.TkElePlotter(collections.TkEleL2, egid_iso_etatk_selections),
+    plotters.EGPlotter(collections.TkEmEE, sta_selection),
+    plotters.EGPlotter(collections.TkEmEB, sta_selection),
+    plotters.EGPlotter(collections.EGStaEE, sta_selection),
+    plotters.EGPlotter(collections.EGStaEB, sta_selection),
     ]
+
+quantization_plotters = [
+    plotters.QuantizationPlotter(collections.hgc_cl3d,  simple_selections, ['pt', 'hoe', 'srrtot', 'meanz_scaled']),
+    plotters.QuantizationPlotter(collections.tracks,  simple_selections, ['nStubs', 'chi2'])
+
+]
+
+# l1tc_pho_plotters = [
+
+# ]
